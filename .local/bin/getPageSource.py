c6b1dd76c902266c17d66b12a66c5b437aced89a
hache <null>
2022-12-04 22:05:06 +0100

    Added some scripts

---
+++ .local/bin/getPageSource.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+site=sys.argv[1]
+sourceFile=sys.argv[2]
+
+options = webdriver.FirefoxOptions()
+options.headless = True
+
+driver = webdriver.Firefox(options = options)
+driver.get(site)
+
+pageSource = driver.page_source
+
+fileToWrite = open(sourceFile, "w")
+fileToWrite.write(pageSource)
+fileToWrite.close()
+
+driver.quit()
+os.remove('geckodriver.log')
